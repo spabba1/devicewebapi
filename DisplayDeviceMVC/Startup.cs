1db545ede3452af5bd99ee131752bcf2e3c6fe3d
spabba1 <pabbas.sai@gmail.com>
2017-09-13 08:54:01 -0800

    Second commit

---
+++ DisplayDeviceMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DisplayDeviceMVC.Startup))]
+namespace DisplayDeviceMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ devicewebapi/Controllers/DeviceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dataaccess;
+using System.Web.Script.Serialization;
+
+
+namespace devicewebapi.Controllers
+{
+    public class DeviceController : ApiController
+    {
+        public IEnumerable<Device_details> Get()
+        {
+            Console.WriteLine("hi 75F");
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            using (CRUDexEntities entities = new CRUDexEntities())
+            {
+                // IEnumerable<Device_details> List = entities.Device_details.ToList();
+                //return json.Serialize(entities.Device_details.ToList());
+                return entities.Device_details.ToList();
+               
+            }
+        }
+    }
+}
